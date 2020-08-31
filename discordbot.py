2d2387cc0d0d2de2a2db52da7e56e4f3bb3ff945
neohachannel <61834989+neohachannel@users.noreply.github.com>
2020-08-31 19:27:07 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -21,9 +21,4 @@ async def おやす(ctx):
 async def HPB(ctx):
     await ctx.send('お誕生日おめでとうございます！！')
 
-citycode = '016010'
-resp = urllib.request.urlopen('http://weather.livedoor.com/forecast/webservice/json/v1?city=%s'%citycode).read()
-resp = json.loads(resp.decode('utf-8'))
-
-
 bot.run(token)
