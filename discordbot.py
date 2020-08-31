b3515836530101e29eda0e59b0c2548f98ab11fd
neohachannel <61834989+neohachannel@users.noreply.github.com>
2020-08-31 19:11:00 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -21,5 +21,25 @@ async def おやす(ctx):
 async def HPB(ctx):
     await ctx.send('お誕生日おめでとうございます！！')
 
+citycode = '016010'
+resp = urllib.request.urlopen('http://weather.livedoor.com/forecast/webservice/json/v1?city=%s'%citycode).read()
+resp = json.loads(resp.decode('utf-8'))
+
+@client.event
+async def on_ready():
+  print("logged in as " + client.user.name)
+
+@client.event
+async def on_message(message):
+  if message.author != client.user:
+
+    if message.content == "Bot君、札幌の天気は？":
+      msg = resp['location']['city']
+      msg += "の天気は、\n"
+      for f in resp['forecasts']:
+        msg += f['dateLabel'] + "が" + f['telop'] + "\n"
+      msg += "です。"
+
+      await client.send_message(message.channel, message.author.mention + msg)
 
 bot.run(token)
